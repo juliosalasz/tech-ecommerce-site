899e51e0cd2fc1a6025cb74ab0671fcd1958266e
Julio Salas <jscomic@gmail.com>
2022-05-30 19:56:59 -0500

    featuredItem progress

--- src/components/featuredMenuItems/FeaturedMenuItems.jsx
+++ src/components/featuredMenuItems/FeaturedMenuItems.jsx
@@ -1,3 +1,18 @@
-const FeaturedMenuItems = () => {};
+import "./featuredMenuItems.css";
+
+const FeaturedMenuItems = ({ item }) => {
+  console.log(item.name);
+  return (
+    <div
+      className={`featuredMenuItems ${item.size}`}
+      style={{ backgroundImage: `url(${item.imageUrl})` }}
+    >
+      <div className="menuItemInfo">
+        <h2>{item.name}</h2>
+        <h3>{item.price}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default FeaturedMenuItems;
--- src/route/homepage/featuredItems/FeaturedItems.jsx
+++ src/route/homepage/featuredItems/FeaturedItems.jsx
@@ -1,14 +1,23 @@
 import { FeaturedList } from "../../../utils/featuredList";
 
+import FeaturedMenuItems from "../../../components/featuredMenuItems/FeaturedMenuItems";
+
 import "./featuredItemsStyle.css";
 
 const FeaturedItems = () => {
   return (
-    <div>
-      <h2>Featured Items List</h2>
-      <div></div>
-      <h2>Brands Marquee</h2>
-    </div>
+    <section className="featuredSection">
+      <div className="featuredContent">
+        <h2 className="title">POPULAR PRODUCTS</h2>
+        <div className="featuredList">
+          {FeaturedList.map((item) => (
+            <FeaturedMenuItems key={item.id} item={item} />
+          ))}
+        </div>
+
+        <h2>Brands Marquee</h2>
+      </div>
+    </section>
   );
 };
 
