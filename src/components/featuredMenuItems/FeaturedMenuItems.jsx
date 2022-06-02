6a23bded98adad07bc731dfee830be245049f229
Julio Salas <jscomic@gmail.com>
2022-06-01 20:55:31 -0500

    featured list with hidden names

--- src/components/featuredMenuItems/FeaturedMenuItems.jsx
+++ src/components/featuredMenuItems/FeaturedMenuItems.jsx
@@ -1,13 +1,13 @@
 import "./featuredMenuItems.css";
 
 const FeaturedMenuItems = ({ item }) => {
-  console.log(item.name);
+  const largeImage = item.size ? "itemLarge" : null;
   return (
     <div
       className={`featuredMenuItems ${item.size}`}
       style={{ backgroundImage: `url(${item.imageUrl})` }}
     >
-      <div className="menuItemInfo">
+      <div className={`menuItemInfo ${largeImage}`}>
         <h2>{item.name}</h2>
         <h3>{item.price}</h3>
       </div>
